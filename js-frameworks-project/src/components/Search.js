ef62ab67691b4ddbbdc93acb44eae98fcb2dceac
Silje Langrud <silje.langrud@hotmail.com>
2021-12-14 20:22:27 +0100

    Added search bar on homepage

---
+++ js-frameworks-project/src/components/Search.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { API } from '../constants/api';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export const Search = () => {
+    const [pokeCards, setCards] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [searchResults, sortedCards] = useState([]);
+
+    useEffect(() => {
+        // const cardURL = `${API}properties`;
+        async function loadCards() {
+          try {
+            const response = await fetch(API);
+
+            if (response.ok) {
+              const results = await response.json();
+              const json = results.cards;
+              
+              setCards(json);
+            } else {
+              setError("No results to view.");
+            }
+          } catch (error) {
+            setError(error.toString());
+          } finally {
+            setLoading(false);
+          }
+        }
+        loadCards();
+      }, []);
+      if (loading) {
+        return <div>
+            <span className="visually-hidden">Loading...</span>
+        </div>;
+      }
+      if (error) {
+        return <div>Unable to locate cards.</div>;
+      }
+
+      const searching = (e) => {
+        const inputValue = e.target.value.trim().toLowerCase();
+        const sorted = Object.values(pokeCards).filter((pokeCard) =>
+        pokeCard.name.toLowerCase().includes(inputValue)
+        );
+        sortedCards(sorted);
+      };
+
+    return (
+        <>
+            <input onChange={(e) => searching(e)} placeholder="Search card name" aria-label="Search card name"></input>
+            <ul>
+            {searchResults.map((pokeCard) => {
+              let {
+                id,
+                name,
+                supertype,
+              } = pokeCard;
+              
+              return (
+                <>
+                <Link to={`/cards/${id}`}>
+                  <li 
+                    key={id}
+                    id={id}
+                    name={name}
+                    supertype={supertype}
+                  >
+                    {pokeCard.name}
+                  </li>
+                </Link>
+                </>
+              );
+            })}
+            </ul>
+        </>
+    );
+};
+
+// export default Search
--- js-frameworks-project/src/layout/Homepage.js
+++ js-frameworks-project/src/layout/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Heading from '../components/Heading'
 import Results from '../components/Results'
+import { Search } from '../components/Search'
 
 function Homepage() {
     return (
         <div>
-            <Heading title="Test"/>
+            <Heading title="Homepage"/>
+            <Search />
             <Results />
         </div>
     )
